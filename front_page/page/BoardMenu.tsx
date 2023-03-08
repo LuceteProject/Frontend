cacedbced5a709973d001bbe3035e03e705a9fe0
jha2ee <jha2ee99@gmail.com>
2023-03-09 02:44:58 +0900

    nav add
    되는지 확인안됨

--- front_page/page/BoardMenu.tsx
+++ front_page/page/BoardMenu.tsx
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 import React, { useState } from 'react';
-
 import {
   DesktopOutlined,
   FileOutlined,
