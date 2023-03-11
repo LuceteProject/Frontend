d634f3abfd03de47571c2d3a5290f1c43f04d7c6
jha2ee <jha2ee99@gmail.com>
2023-03-12 02:01:02 +0900

    Create PersonalSetting.tsx
    페이지 [Setting>Personal Setting] 레이아웃
    내용 보완 예정

---
+++ front_page/pages/PersonalSetting.tsx
@@ -0,0 +1,100 @@
+import {
+  Button,
+  Icon,
+  List,
+  Switch,
+  WhiteSpace,
+  WingBlank,
+} from '@ant-design/react-native'
+import React, { useEffect, useState } from 'react'
+import { ScrollView } from 'react-native'
+
+const SettingScreen = () => {
+  
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect( () => {
+    console.log("changed");
+  }, [disabled]);
+
+  return (
+    <ScrollView>
+      <List renderHeader="알림">
+        <List.Item extra={<Switch />}>공지사항</List.Item>
+        <List.Item extra={<Switch />}>일정</List.Item>
+        <List.Item extra={<Switch />}>쪽지</List.Item>
+      </List>
+      <List renderHeader="화면">
+        <WhiteSpace />
+        <WingBlank>
+          <Button type="primary" onPress={()=> setDisabled(!disabled)}>
+            글씨 크기
+          </Button>
+        </WingBlank>
+      </List>
+      <WhiteSpace />
+      <List>
+        <List.Item extra={<Switch checked color="black" />}>
+          다크모드
+        </List.Item>
+      </List>
+    </ScrollView>
+  )
+};
+
+
+export default SettingScreen;
+
+/*
+
+import {
+    Button,
+    Icon,
+    List,
+    Switch,
+    WhiteSpace,
+    WingBlank,
+  } from '@ant-design/react-native'
+  import React from 'react'
+  import { ScrollView } from 'react-native'
+  
+  export default class SwitchExample extends React.Component<any, any> {
+    constructor(props: any) {
+      super(props)
+      this.state = {
+        disabled: true,
+      }
+    }
+  
+    toggle = () => {
+      this.setState({
+        disabled: !this.state.disabled,
+      })
+    }
+    render() {
+      return (
+        <ScrollView>
+          <List renderHeader="알림">
+            <List.Item extra={<Switch />}>공지사항</List.Item>
+            <List.Item extra={<Switch />}>일정</List.Item>
+            <List.Item extra={<Switch />}>쪽지</List.Item>
+          </List>
+          <List renderHeader="화면">
+            <WhiteSpace />
+            <WingBlank>
+              <Button type="primary" onPress={this.toggle}>
+                글씨 크기
+              </Button>
+            </WingBlank>
+          </List>
+          <WhiteSpace />
+          <List>
+            <List.Item extra={<Switch checked color="black" />}>
+              다크모드
+            </List.Item>
+          </List>
+        </ScrollView>
+      )
+    }
+  }
+  */
