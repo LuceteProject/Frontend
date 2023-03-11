ec2a1d0a94d9330845ed33af3a88c4d6e8b13317
jha2ee <50551231+jha2ee@users.noreply.github.com>
2023-03-11 18:40:53 +0900

    Create layout_important.tsx
    개발 파일 형식 지정

---
+++ front_page/pages/layout_important.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+
+/* 사용할 컴포넌트 여기에서 import */
+// Ex: import { NavigationContainer } from '@react-navigation/native';
+
+/* functional execution */
+const App = () => {
+	return 
+        <View>{
+        
+        /*화면 내용 여기에 */
+
+        }</View>
+}
+
+
+/* style 형식 지정은 여기에서 */
+const styles = StyleSheet.create({
+        /* 
+        항목 이름 : {
+                속성이름 : 값
+        },
+
+        컴포넌트 별 구분 가능
+        */
+        centeredView: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: 22,
+              },
+});
+
+/* page return */
+export default App;
