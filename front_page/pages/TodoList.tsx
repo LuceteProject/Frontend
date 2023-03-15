956f0bef9471054c101de57c83c0ad391abc721e
jha2ee <jha2ee99@gmail.com>
2023-03-15 21:38:10 +0900

    Todo list layout
    있는거 주워왔는데 실행은 잘 안됨

--- front_page/pages/TodoList.tsx
+++ front_page/pages/TodoList.tsx
@@ -1,22 +1,22 @@
 // tslint:disable:no-empty
 import React, { useEffect, useState } from 'react'
+import { StyleSheet, ScrollView, Text, View, SafeAreaView, KeyboardAvoidingView, Alert, Keyboard, Platform, TextInput } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import { StyleSheet, Image, ScrollView, Text, View, FlatList, SafeAreaView, KeyboardAvoidingView } from 'react-native'
-import { List, NoticeBar } from '@ant-design/react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+import Task from '../components/Task';
 
-/* 
-List 항목을 좀 더 깔끔하게 표현하기 위해 만든 컴포넌트
-List.Item List.Item.Brief 대신 사용 
-*/
-const Item = List.Item
-const Brief = Item.Brief
+const Stack = createNativeStackNavigator();
 
 /* Screen */
 const TodoList = () => {
   /* values from API 
   유저 정보에서 받아올 내용 : 기수/이름/팀/역할/상메/번호/메일 
   */
-  //const [items, setItems] = useState([]);
+
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
   /* 분리
   const itemHandler= () => {
@@ -24,8 +24,19 @@ const TodoList = () => {
   
     setItems(item_);
   };
-  
+    
   */
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = ({ index }: any) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
 
   /* For Data fetch from server
   useEffect(() => {
@@ -40,98 +51,97 @@ const TodoList = () => {
   fetchContentData();
   }, []);
   */
+  const Main = ({ navigation }: any) => {
+    return (
+      <View style={styles.container}>
+        {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1
+          }}
+          keyboardShouldPersistTaps='handled'
+        >
+
+          {/* Today's Tasks */}
+          <View style={styles.tasksWrapper}>
+            <View style={styles.items}>
+              {/* This is where the tasks will go! */}
+              {
+                taskItems.map((item, index) => {
+                  return (
+                    <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                      <Task text={item} />
+                    </TouchableOpacity>
+                  )
+                })
+              }
+              
+            </View>
+
+          </View>
+
+        </ScrollView>
+
+        {/* Write a task */}
+        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder={'Write a task'}
+            value={task}
+          //onChangeText={text => setTask(text)}
+          />
+          <TouchableOpacity onPress={() =>
+            handleAddTask()
+            //console.log('add task')
+          }>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+
+      </View>
+    );
+  }
+
   return (
     <>
-      <ScrollView
-        style={styles.background}
-        // Todo : 아래 내용은 나도 찾아봐야함
-        automaticallyAdjustContentInsets={false}
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}>
+      <Stack.Navigator
+        screenOptions={{
+          headerRight: () => (
+            <>
+              <TouchableOpacity
+                // Notification icon - components 분리할 수 있으면 뺴기
+                onPress={() => {
+                  // 왜 안눌리지
+                  Alert.alert("pressed!");
+                }}>
+                <Icon name="notifications" size={30} color="#000"
+                  style={{
+                    // 둥근 원 테두리, 근데 배경 없으면 필요없을듯?
+                    width: 40,
+                    height: 40,
+                    borderRadius: 100,
+                  }}
+                />
+              </TouchableOpacity>
+            </>
+          )
+          // only in iOS - headerBackTitleVisible='false'
+        }}
+      >
+        <Stack.Screen
+          name="To Do List"
+          component={Main}
+        />
 
-        <List renderHeader={'전체'}>
-          <Item
-            // 하나의 Item에 한 명씩 정보 불러오기
-            // thumb : image link, extra : 오른쪽에 sub로 들어가는 text, multipleLine : 여러줄 가능
-            thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png"
-            extra={
-              <View>
-                <Brief style={{ textAlign: 'right' }}>01012345678</Brief>
-                <Brief style={{ textAlign: 'right' }}>mail@gmail.com</Brief>
-              </View>}
-            // View Component 안에 <Brief> 커스텀 컴포넌트 사용해 추가내용 넣음
-            multipleLine>
-            기수 이름
-            <Brief>회장</Brief>
-            <Brief style={{ color: 'blue' }}>"상태메시지"</Brief>
-          </Item>
-
-
-        </List>
-        <List renderHeader={'팀' /*팀이름 여기에*/}>
-          <Item
-            // 하나의 Item에 한 명씩 정보 불러오기
-            // thumb : image link, extra : 오른쪽에 sub로 들어가는 text, multipleLine : 여러줄 가능
-            thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png"
-            extra={<View>
-              <Brief style={{ textAlign: 'right' }}>01012345678</Brief>
-              <Brief style={{ textAlign: 'right' }}>mail@gmail.com</Brief>
-            </View>}
-            // View Component 안에 <Brief> 커스텀 컴포넌트 사용해 추가내용 넣음
-            multipleLine>
-            기수 이름
-            <Brief>팀장</Brief>
-            <Brief style={{ color: 'blue' }}>"상태메시지"</Brief>
-          </Item>
-
-          <Item
-            thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png"
-            extra={<View>
-              <Brief style={{ textAlign: 'right' }}>01012345678</Brief>
-              <Brief style={{ textAlign: 'right' }}>mail@gmail.com</Brief>
-            </View>}
-            multipleLine>
-            기수 이름
-            <Brief>무슨 팀</Brief>
-            <Brief style={{ color: 'blue' }}>"길이 제한 20자?"</Brief>
-          </Item>
-
-        </List>
-        <List renderHeader={'개인' /*팀이름 여기에*/}>
-          <Item
-            // 하나의 Item에 한 명씩 정보 불러오기
-            // thumb : image link, extra : 오른쪽에 sub로 들어가는 text, multipleLine : 여러줄 가능
-            thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png"
-            extra={<View>
-              <Brief style={{ textAlign: 'right' }}>01012345678</Brief>
-              <Brief style={{ textAlign: 'right' }}>mail@gmail.com</Brief>
-            </View>}
-            // View Component 안에 <Brief> 커스텀 컴포넌트 사용해 추가내용 넣음
-            multipleLine>
-            기수 이름
-            <Brief>팀장</Brief>
-            <Brief style={{ color: 'blue' }}>"상태메시지"</Brief>
-          </Item>
-
-          <Item
-            thumb="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png"
-            extra={<View>
-              <Brief style={{ textAlign: 'right' }}>01012345678</Brief>
-              <Brief style={{ textAlign: 'right' }}>mail@gmail.com</Brief>
-            </View>}
-            multipleLine>
-            기수 이름
-            <Brief>무슨 팀</Brief>
-            <Brief style={{ color: 'blue' }}>"길이 제한 20자?"</Brief>
-          </Item>
-
-        </List>
-
-      </ScrollView>
-      <View
-        style={{ paddingBottom: 20 }}>
-      </View>
+      </Stack.Navigator>
     </>
+
   )
 }
 
@@ -143,9 +153,47 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f5f5f9',
   },
-
   container: {
-    // for empty space in iOS
-    height: 10
+    flex: 1,
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
