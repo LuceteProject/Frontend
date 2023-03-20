2d8715973f3841bb245845a1457422f229a4aa1b
jha2ee <50551231+jha2ee@users.noreply.github.com>
2023-03-20 17:12:26 +0900

    출결 페이지 수정
    화면전환 기능
    출결목록 클릭시 화면 바꾸기

--- front_page/pages/Attendance.tsx
+++ front_page/pages/Attendance.tsx
@@ -7,6 +7,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 
+import ManagePage from './ManageAttendance';
+
 const Stack = createNativeStackNavigator();
 
 const Screen = () => {
@@ -46,6 +48,7 @@ const Screen = () => {
             <>
                 <View
                     //View for select the sub menu
+                    /* 이거 뭔가 그럴듯해 보이게 스타일 지정하기!! */
                     style={
                         {
                             margin: 10,
@@ -57,7 +60,11 @@ const Screen = () => {
                         {/* 비활성화일때 텍스트 효과 바꾸기 */}
                         <Text>출석 확인</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={clickHandler}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            /* 1. Navigate to the Details route with params */
+                            navigation.navigate('출석기록확인');
+                        }}>
                         <Text>출석 기록</Text>
                     </TouchableOpacity>
                 </View>
@@ -80,16 +87,31 @@ const Screen = () => {
                             marginTop: 30,
                             marginBottom: 30,
                         }}>
-                        출석이 확인되었습니다. (상황에 따라 값 변경)</Text>
+                        출석이 확인되었습니다. (상황에 따라 값 변경)
+                        {/* {message} 위 QR코드를 출결 관리자에게 보여주세요. */}
+                    </Text>
                     <Button
+                        // 위에 탭으로 대체 예정
                         title='내 출석 기록 확인(삭제예정)'
                         onPress={() => {
                             /* 1. Navigate to the Details route with params */
                             navigation.navigate('출석기록확인');
-                          }}
+                        }}
                     />
-                </View>
 
+                </View>
+                <TouchableOpacity
+                    /* 관리자에게만 보이게 권한 설정 */
+                    activeOpacity={0.7}
+                    onPress={() => {
+                        navigation.navigate('출결관리');
+                    }}
+                    style={styles.touchableOpacityStyle}>
+                    <Icon name="people" size={60} color="#000"
+                        style={styles.floatingButtonStyle}
+                    />
+                </TouchableOpacity>
+ 
 
             </>
 
@@ -100,6 +122,24 @@ const Screen = () => {
     const CheckRecord = ({ navigation }: any) => {
         return (
             <View>
+                <View
+                    //View for select the sub menu
+                    style={
+                        {
+                            margin: 10,
+                            flexDirection: 'row', //정렬 방향
+                            justifyContent: 'space-around'
+                        }
+                    }>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        {/* 비활성화일때 텍스트 효과 바꾸기 */}
+                        <Text>출석 확인</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={clickHandler}>
+                        <Text>출석 기록</Text>
+                    </TouchableOpacity>
+                </View>
                 <Text>출석 기록 확인</Text>
                 <Button title="Go back" onPress={() => navigation.goBack()} />
 
@@ -111,6 +151,7 @@ const Screen = () => {
         <>
             <Stack.Navigator
                 screenOptions={{
+                    headerShown: false,
                     headerRight: () => (
                         <>
                             <TouchableOpacity
@@ -130,6 +171,7 @@ const Screen = () => {
                         </>
 
                     )
+
                     // only in iOS - headerBackTitleVisible='false'
                 }}
             >
@@ -140,6 +182,15 @@ const Screen = () => {
                 <Stack.Screen
                     name="출석기록확인"
                     component={CheckRecord}
+                    options={
+                        {
+                            presentation: 'containedModal',
+                        }
+                    }
+                />
+                <Stack.Screen
+                    name="출결관리"
+                    component={ManagePage}
                 />
 
 
@@ -162,5 +213,18 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
     },
-
+    /* 밑에 두개 floating button style
+    이거 왜 가운데에 안오냐 ....? ㅁㄹ...*/
+    touchableOpacityStyle: {
+        position: 'absolute',
+        width: 80,
+        height: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+        right: 30,
+        bottom: 30,
+    },
+    floatingButtonStyle: {
+       backgroundColor: '#fff'
+    },
 });
