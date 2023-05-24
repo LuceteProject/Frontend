2b516bc4c64c25e9ee1e33fbed8447ad9bca4213
jha2ee <jha2ee99@gmail.com>
2023-05-24 22:04:45 +0900

    Update Notification.tsx
    탭 라이브러리 변경
    레이아웃 추가

--- front_page/pages/Notification.tsx
+++ front_page/pages/Notification.tsx
@@ -1,17 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, Image, ScrollView, Text, View, Alert, Button, TouchableOpacity, ActivityIndicator, Modal, KeyboardAvoidingView } from 'react-native'
+import { TabView, SceneMap } from 'react-native-tab-view';
 
-import { Tab } from '@rneui/themed';
-
-const Page = () => {
+const Page = ({ navigation }: any) => {
     const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: 'first', title: '알림' },
+        { key: 'second', title: '쪽지함' },
+    ]);
+
+    const FirstRoute = (props: any) => (
+        <View style={{ height: 500 }}>
+            <View
+                id="section_notification"
+                style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text>알림 위치</Text>
+            </View>
+            {/* nav에 navigation 넣어줘야 하고.. 이걸 props로 받아야 하고... 이건 가장먼저 SceneMap에서 넣어줘야하고... api에서 불러오면 값하나로 통일될듯?*/}
+        </View>
+    );
+    const SecondRoute = (props: any) => (
+        <View style={{ height: 500 }}>
+            <View
+                id="section_messanger"
+                style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text>쪽지함</Text>
+            </View>
+
+        </View>
+    );
+
+    //Tab View 항목 지정
+    const renderScene = SceneMap({
+        first: () => <FirstRoute nav={navigation} title="sample" author="lee" wtime="2023/01/05" reply='3' />,
+        second: () => <SecondRoute nav={navigation} title="sample" author="lee" wtime="2023/01/05" reply='3' />,
+    });
 
     return (
         <>
-            <Tab value={index} onChange={setIndex} dense>
-                <Tab.Item>알림</Tab.Item>
-                <Tab.Item>쪽지함</Tab.Item>
-            </Tab>
+            <TabView
+                //renderTabBar
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+            />
         </>
     )
 }
