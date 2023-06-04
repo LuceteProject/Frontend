818e71cc7d97850dddd0e633f8aca504ca17272b
jha2ee <jha2ee99@gmail.com>
2023-06-04 22:53:09 +0900

    Update TodoScreen.tsx
    1. 화면 구성 업데이트
    2. 항목 추가 및 삭제 가능
    3. 디자인 수정 필요

--- front_page/pages/TodoScreen.tsx
+++ front_page/pages/TodoScreen.tsx
@@ -1,140 +1,152 @@
-// tslint:disable:no-empty
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, ScrollView, Text, View, KeyboardAvoidingView, Alert, Keyboard, Platform, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, Text, View, KeyboardAvoidingView, TouchableOpacity, Alert, Keyboard, TextInput, Button, FlatList, Switch } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
 import Icon from 'react-native-vector-icons/Ionicons';
 import Task from '../components/Task';
 
 const Stack = createNativeStackNavigator();
 
-/* Screen */
+interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+  category: string;
+}
+
 const Screen = ({ navigation }: any) => {
-  /* values from API 
-  유저 정보에서 받아올 내용 : 기수/이름/팀/역할/상메/번호/메일 
-  */
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodoAll, setNewTodoAll] = useState('');
+  const [newTodoTeam, setNewTodoTeam] = useState('');
+  const [newTodoPersonal, setNewTodoPersonal] = useState('');
+  const [category, setCategory] = useState('전체');
 
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+  const handleAddTodo = () => {
+    let newTodo = '';
+    if (category === '전체') {
+      newTodo = newTodoAll;
+      setNewTodoAll('');
+    } else if (category === '팀') {
+      newTodo = newTodoTeam;
+      setNewTodoTeam('');
+    } else if (category === '개인') {
+      newTodo = newTodoPersonal;
+      setNewTodoPersonal('');
+    }
 
-  /* 분리
-   const itemHandler= () => {
-    let item_ = [...items];
-  
-    setItems(item_);
+    if (newTodo) {
+      const newTodoItem: Todo = {
+        id: Date.now().toString(),
+        text: newTodo,
+        completed: false,
+        category: category,
+      };
+      setTodos([...todos, newTodoItem]);
+    }
   };
-    
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
-  const completeTask = ({ index }: any) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy)
-  }
 
-
-  */
-  
-  /* For Data fetch from server
-  useEffect(() => {
-    const fetchContentData = async () => {
-      try {
-          
-      } catch (err) {
-          
-      }
+  const handleToggleTodo = (id: string) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
   };
-  
-  fetchContentData();
-  }, []);
-  */
+
+  const handleDeleteTodo = (id: string) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const renderItem = ({ item }: { item: Todo }) => (
+    <View style={styles.itemContainer}>
+      <Text
+        style={[
+          styles.itemText,
+          item.completed && styles.completedText,
+        ]}
+      >
+        {item.text}
+      </Text>
+      <Switch value={item.completed} onValueChange={() => handleToggleTodo(item.id)} />
+      <TouchableOpacity onPress={() => handleDeleteTodo(item.id)}>
+        <Icon name="close" size={15} color="red" />
+      </TouchableOpacity>
+    </View>
+  );
+
   const Main = () => {
     return (
-      <View style={styles.container}>
-        {/* Added this scroll view to enable scrolling when list gets longer than the page */}
-        <ScrollView
-          contentContainerStyle={{
-            flexGrow: 1
-          }}
-          keyboardShouldPersistTaps='handled'
-        >
-
-          {/* Today's Tasks */}
-          <View style={styles.tasksWrapper}>
-            <View style={styles.items}>
-              {/* This is where the tasks will go! */}
-              {
-                taskItems.map((item, index) => {
-                  return (
-                    <TouchableOpacity key={index} 
-                    //onPress={() => completeTask(index)}
-                    >
-                      <Task text={item} />
-                    </TouchableOpacity>
-                  )
-                })
-              }
-              
-            </View>
-
-          </View>
-
-        </ScrollView>
-
-        {/* Write a task */}
-        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          style={styles.writeTaskWrapper}
-        >
+      <View style={{margin: 5}}>
+        <Text>전체</Text>
+        <View style={styles.inputContainer}>
           <TextInput
             style={styles.input}
-            placeholder={'Write a task'}
-            value={task}
-            //onChangeText={text => setTask(text)}
+            placeholder="전체 항목을 입력하세요"
+            value={newTodoAll}
+            onChangeText={text => setNewTodoAll(text)}
+            onSubmitEditing={handleAddTodo}
           />
-          <TouchableOpacity onPress={() =>
-            //handleAddTask()
-            console.log('add task')
-          }>
-            <View style={styles.addWrapper}>
-              <Text style={styles.addText}>+</Text>
-            </View>
-          </TouchableOpacity>
-        </KeyboardAvoidingView>
+          <Button title="추가" onPress={() => { handleAddTodo(); setCategory('전체'); }} />
+        </View>
+        <View>
 
+          <FlatList
+            data={todos.filter((todo) => todo.category === '전체')}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+
+        <Text>팀</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="팀 항목을 입력하세요"
+            value={newTodoTeam}
+            onChangeText={text => setNewTodoTeam(text)}
+            onSubmitEditing={handleAddTodo}
+          />
+          <Button title="추가" onPress={() => { handleAddTodo(); setCategory('팀'); }} />
+        </View>
+        <View>
+
+          <FlatList
+            data={todos.filter((todo) => todo.category === '팀')}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+        <Text>개인</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="개인 항목을 입력하세요"
+            value={newTodoPersonal}
+            onChangeText={text => setNewTodoPersonal(text)}
+            onSubmitEditing={handleAddTodo}
+          />
+          <Button title="추가" onPress={() => { handleAddTodo(); setCategory('개인'); }} />
+        </View>
+        <View>
+
+          <FlatList
+            data={todos.filter((todo) => todo.category === '개인')}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
       </View>
     );
-  }
+  };
 
   return (
     <>
       <Stack.Navigator
         screenOptions={{
           headerRight: () => (
-            <>
-              <TouchableOpacity
-                // Notification icon - components 분리할 수 있으면 뺴기
-                onPress={() => {
-                  // 왜 안눌리지
-                  Alert.alert("pressed!");
-                }}>
-                <Icon name="notifications" size={30} color="#000"
-                  style={{
-                    // 둥근 원 테두리, 근데 배경 없으면 필요없을듯?
-                    width: 40,
-                    height: 40,
-                    borderRadius: 100,
-                  }}
-                />
-              </TouchableOpacity>
-            </>
+            <TouchableOpacity onPress={() => Alert.alert("pressed!")}>
+              <Icon name="notifications" size={30} color="#000" />
+            </TouchableOpacity>
           )
-          // only in iOS - headerBackTitleVisible='false'
         }}
       >
         <Stack.Screen
@@ -142,64 +154,45 @@ const Screen = ({ navigation }: any) => {
           component={Main}
           options={{
             title: '투두리스트',
-        }
-        }
+          }}
         />
-
       </Stack.Navigator>
     </>
   );
-}
-
+};
 
 export default Screen;
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: '#f5f5f9',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#E8EAED',
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
+  inputContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 20,
+    padding: 5
   },
   input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
+    flex: 1,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
     backgroundColor: '#FFF',
-    borderRadius: 60,
+    borderRadius: 10,
     borderColor: '#C0C0C0',
     borderWidth: 1,
-    width: 250,
   },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
+  itemContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
+    marginLeft: 10,
+    marginRight: 10
   },
-  addText: {},
+  itemText: {
+    flex: 1,
+    textDecorationLine: 'none',
+    fontSize: 16,
+    marginHorizontal: 10,
+  },
+  completedText: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
+  },
 });
